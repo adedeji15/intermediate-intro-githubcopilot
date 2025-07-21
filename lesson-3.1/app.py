2080c15a57e84ba03946397687f8d77dd013b9ae
adedeji15 <adedeji.adeolubalogun@gmail.com>
2025-07-21 11:30:20 -0500

    Update app.py

---
+++ lesson-3.1/app.py
@@ -0,0 +1,8 @@
+import os
+os.environ["DISPLAY"] = ":1"
+import turtle
+
+wn = turtle.Screen()  # Creates the game window
+player = turtle.Turtle()  # Creates a turtle
+
+wn.mainloop()  # Keeps the window open
